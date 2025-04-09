d3dae392524cce5c57622c3fcac305ad190ed4b1
sumit923s <ssingh7296003817@gmail.com>
2025-04-09 22:13:02 +0530

    “Merged”

--- backend/admin/assets/inc/config.php
+++ backend/admin/assets/inc/config.php
@@ -4,5 +4,5 @@ $dbpass="";
 $host="localhost";
 $db="hmisphp";
 // $mysqli=new mysqli($host,$dbuser, $dbpass, $db);
-$mysqli = new mysqli('127.0.0.1', 'root', '7296003817@Ss', 'hmisphp');
+$mysqli = new mysqli('127.0.0.1', 'hmsuser', "Strong@Pass123", 'hmisphp');
 ?>
--- backend/doc/assets/inc/config.php
+++ backend/doc/assets/inc/config.php
@@ -4,6 +4,6 @@ $dbpass="";
 $host="localhost";
 $db="hmisphp";
 // $mysqli=new mysqli($host,$dbuser, $dbpass, $db);
-$mysqli = new mysqli('127.0.0.1', 'root', '7296003817@Ss', 'hmisphp');
+$mysqli = new mysqli('127.0.0.1', 'hmsuser', "Strong@Pass123", 'hmisphp');
 ?>
 
